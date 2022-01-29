452d0d90c35872686597b9f331788f88919ee757
Alan Galicia <agaliciar4@miumg.edu.gt>
2022-01-28 19:40:38 -0600

    calculadora1

---
+++ primertarea.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+float main()
+{
+    float num1;
+    cout << "Introduce el primer número para realizar operaciones: ";
+    cin >> num1;
+
+
+    return 0;
+}
