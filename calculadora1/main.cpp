6d7fef97628a08b97c16dbf1bbd9c41fc3dbdd48
Alan Galicia <agaliciar4@miumg.edu.gt>
2022-01-28 20:34:40 -0600

    segundo numero, suma y resta actualizado

--- calculadora1/main.cpp
+++ calculadora1/main.cpp
@@ -4,9 +4,12 @@ using namespace std;
 
 int main()
 {
-    int num1;
+    int num1, num2;
     cout << "introduce el primer número para las operaciones matemáticas: ";
     cin >> num1;
-
+    cout << "introduce el primer número para las operaciones matemáticas: ";
+    cin >> num2;
+    cout << "La suma de los numeros es: " << num1 + num2 << endl;
+    cout << "La resta de los numeros es: " << num1 - num2 << endl;
     return 0;
 }
