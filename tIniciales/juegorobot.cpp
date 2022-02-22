f75bd85599c65c782ee7f8eb59401edc101231a5
Alan Galicia <agaliciar4@miumg.edu.gt>
2022-02-21 20:09:32 -0600

    tarea del robor con archivos

--- tIniciales/juegorobot.cpp
+++ tIniciales/juegorobot.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 void drawMap(int posX,int posY,char gameMap[10][10]){
   for(int i=0;i<10;i++){
@@ -15,7 +16,14 @@ void drawMap(int posX,int posY,char gameMap[10][10]){
     cout<<endl;
   }
 }
-int main(){
+int main()
+{
+  ///creación de archivo
+  ofstream myfile ("gameData.txt");
+  string nom = "";
+  cout<<"Bienvenido/a!!! Ingrese su nombre: "<<endl;
+  cin >>nom;
+  int iz=0,der=0,arr=0,ab=0,numIte=0;
   int posX=3;
   int posY=3;
   char map[10][10]={{'1','1','1','1','1','1','1','1','1','1'},
@@ -33,7 +41,7 @@ int main(){
   drawMap(posX,posY,map);
   while(!gameOver)
   {
-    cout << "Bienvenido(a)"  << endl;
+    cout << "Bienvenido(a) "<< nom << endl;
     cout << "Controles Teclas --> w(arriba), a(izquierda), d(derecha) y s(abajo)" << endl;
     cout << "Controles Teclas --> p(Salida)" << endl;
     cin >> teclado;
@@ -42,25 +50,29 @@ int main(){
         case 'a':
             if(posX!=1)
             {
-             posX-=1;up
+             posX-=1;
+             iz+=1;
             }
             break;
         case 'd':
             if(posX!=8)
             {
              posX+=1;
+             der+=1;
             }
             break;
         case 'w':
             if (posY!=1)
             {
               posY-=1;
+              arr+=1;
             }
             break;
         case 's':
             if (posY!=8)
             {
               posY+=1;
+              ab+=1;
             }
             break;
         case 'p':
@@ -70,5 +82,23 @@ int main(){
     }
   drawMap(posX,posY,map);
   }
+  numIte=iz+der+arr+ab;
+  if (myfile.is_open())
+  {
+      myfile<<"Nombre: " << endl;
+      myfile<<nom<<endl;
+      myfile<<"Numero de iteraciones a la izquierda: " << endl;
+      myfile<<iz<<endl;
+      myfile<<"Numero de iteraciones a la derecha: " << endl;
+      myfile<<der<<endl;
+      myfile<<"Numero de iteraciones hacia arriba: " << endl;
+      myfile<<arr<<endl;
+      myfile<<"Numero de iteraciones hacia abajo: " << endl;
+      myfile<<ab<<endl;
+      myfile<<"Numero de iteraciones: " << endl;
+      myfile<<numIte<<endl;
+      myfile.close();
+  }
+  else cout<<"Unable to open file";
  return 0;
 }
