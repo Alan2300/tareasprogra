8df26852847baffe35eda776232e160c8f72cca8
Alan Galicia <agaliciar4@miumg.edu.gt>
2022-03-03 12:18:49 -0600

    subiendo la rama final

--- PrototipoP11P2022/main.cpp
+++ PrototipoP11P2022/main.cpp
@@ -12,10 +12,10 @@ int main()
     for(x=0; x < N; x++)// crea el ciclo for para las filas
     for(y=0; y < N; y++)//crea el ciclo for para las columnas
     if (m1[x][y]%2 == 0)//si al estar m1 en x Y y, se divide dentro de 2 y el residuo es igual a 0, entonces:
-    m2[x][y] = 0;//
+    m2[x][y] = 0;// la acción a relizar si el if se cumple: m2 cuando este en x Y y, sera igual a 0
     else
-    m2[x][y] = 1;
-    printf("%d , %d , %d", m2[0][0], m2[1][1], m2[2][2]);
+    m2[x][y] = 1;//de lo contrario m2 en x Y y, sera 1
+    printf("%d , %d , %d", m2[0][0], m2[1][1], m2[2][2]);//imprimira lo que este en m2 en la fila 1 y columna1, despues en la fila 2 y columna2 y en la fila  y columna3
 
 
 return 0;
