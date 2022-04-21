f3889b8a63d652b082514cb740c63fbc7619f054
Alan Galicia <agaliciar4@miumg.edu.gt>
2022-04-21 12:09:24 -0600

    subiendo submenu de catalogos del sistema

--- segundoParcial/PrototipoP12P2022/main.cpp
+++ segundoParcial/PrototipoP12P2022/main.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 int escribirOpcion();
+int escribirOpcionCatalogo();
 enum Opciones { catalogos = 1, procesos, informes, FIN };
 
 int main()
@@ -15,7 +16,40 @@ int main()
         switch ( opcion ) {
             case catalogos:
                 {
-                    cout <<"yey :)";
+                    enum OpcionesCatalogos { peliculas = 1, salas, asientos, cines, clientes, FIN };
+                    int opcionCatalogos=0;
+                    while (( opcionCatalogos = escribirOpcionCatalogo() ) != FIN)
+                    {
+                        switch ( opcionCatalogos ) {
+                        case peliculas:
+                            {
+                                cout <<"yey :)";
+                            }
+                            break;
+                        case salas:
+                            {
+                                cout <<"yey :)";
+                            }
+                            break;
+                        case asientos:
+                            {
+                                cout <<"yey :)";
+                            }
+                            break;
+                        case cines:
+                            {
+                                cout <<"yey :)";
+                            }
+                            break;
+                        case clientes:
+                            {
+                                cout <<"yey :)";
+                            }
+                            break;
+                            break;
+                        }
+                    }
+
                 }
             break;
             case procesos:
@@ -35,9 +69,9 @@ int main()
 
 int escribirOpcion(){
     system("cls");
-    cout<<"\t\t\t---------------------------------"<<endl;
-	cout<<"\t\t\t |   SISTEMA RECURSOS HUMANOS  |"<<endl;
-	cout<<"\t\t\t---------------------------------"<<endl;
+    cout<<"\t\t\t-------------------------------------------------"<<endl;
+	cout<<"\t\t\t |   ALAN ABIMAEL GALICIA RUANO; 9959-21-3632  |"<<endl;
+	cout<<"\t\t\t-------------------------------------------------"<<endl;
 
     cout<< "\n\n\t\t\t  1. catalogos" << endl
         << "\t\t\t  2. procesos" << endl
@@ -50,4 +84,22 @@ int escribirOpcion(){
 
    return opcionMenu;
 
+}
+int escribirOpcionCatalogo(){
+    system("cls");
+    cout<<"\t\t\t---------------------------------"<<endl;
+    cout<<"\t\t\t |   CATALOGOS DEL SISTEMA  |"<<endl;
+    cout<<"\t\t\t---------------------------------"<<endl;
+    cout<< "\n\n\t\t\t  1. Peliculas" << endl
+    << "\t\t\t  2. Salas" << endl
+    << "\t\t\t  3. Asientos" << endl
+    << "\t\t\t  4. cines" << endl
+    << "\t\t\t  5. clientes" << endl
+    << "\t\t\t  6. Regresar al menu principal" << endl
+    <<"\n\t\t\t---------------------------------"<<endl
+    << "\n\t\t\tIngrese su opcion: ";
+    int opcionMenuCat;
+    cin >> opcionMenuCat;
+
+    return opcionMenuCat;
 }
