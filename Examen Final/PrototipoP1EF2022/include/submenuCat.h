9ce6d43524af624ef4c2e2b8ffa2876129bb7080
Alan Galicia <agaliciar4@miumg.edu.gt>
2022-06-16 11:56:02 -0600

    subiendo submenu de catalogos

---
+++ Examen Final/PrototipoP1EF2022/include/submenuCat.h
@@ -0,0 +1,16 @@
+#ifndef SUBMENUCAT_H
+#define SUBMENUCAT_H
+
+
+class submenuCat
+{
+    public:
+        submenuCat();
+        virtual ~submenuCat();
+
+    protected:
+
+    private:
+};
+
+#endif // SUBMENUCAT_H
